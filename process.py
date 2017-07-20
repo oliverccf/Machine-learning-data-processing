3483650b1368e6c3ca4a3d57aa3d46fdf2ff9fd6
tanvir002700 <tanvir002700@gmail.com>
2017-07-20 23:37:57 +0600

    add pearson's Correlation function

--- process.py
+++ process.py
@@ -1,5 +1,11 @@
 import pandas as PANDA
 
+def pearsons_correlation(x, y):
+    return sum([a[i]*b[i] for i in range(len(a))])/Math.sqrt(sum([x*x for x in a])*sum([y*y for y in b]))
+
 data_frame = PANDA.read_csv('pima-data.csv')
 
-print(data_frame.head(3))
+if data_frame.isnull().values.any():
+    print('data isnt consistance....')
+else:
+    print(data_frame.head(3))
