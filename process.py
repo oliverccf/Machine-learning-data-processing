c883064dfc428de231c465cd87ed6256e34b99fb
tanvir002700 <tanvir002700@gmail.com>
2017-07-29 13:16:27 +0600

    add data cleaning method

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 import pandas as PANDA
 import matplotlib.pyplot as plt
 
+
 def pearsons_correlation(a, b):
     return sum([x*y for x,y in zip(a,b)])/Math.sqrt(sum([x*x for x in a])*sum([y*y for y in b]))
 
@@ -11,7 +12,7 @@ def corr_heatmap(data_frame, size=11):
     heatmap.matshow(correlation)
     plt.xticks(range(len(correlation.columns)), correlation.columns)
     plt.yticks(range(len(correlation.columns)), correlation.columns)
-    plt.show()
+    plt.show("heat map")
 
 
 data_frame = PANDA.read_csv('pima-data.csv')
@@ -20,4 +21,11 @@ if data_frame.isnull().values.any():
     print('data isnt consistance....')
 else:
     corr_heatmap(data_frame)
-    print("ok")
+    print("Enter column name to delete: ")
+    columns = input().split()
+    for column in columns:
+        del data_frame[column]
+        data_frame.head()
+
+    print('After Cleaning......')
+    corr_heatmap(data_frame)
