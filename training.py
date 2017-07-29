49ed67a481e6457116caf89b0c98d1a346a25c58
tanvir002700 <tanvir002700@gmail.com>
2017-07-30 01:48:28 +0600

    find out missing values

--- training.py
+++ training.py
@@ -28,3 +28,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=split_test_s
 print("{0:0.2f}% in training set".format((len(x_train)/len(data_frame.index)) * 100))
 print("{0:0.2f}% in test set".format((len(x_test)/len(data_frame.index)) * 100))
 
+
+print("# rows in dataframe {0}".format(len(data_frame)))
+print("# rows missing glucose_conc: {0}".format(len(data_frame.loc[data_frame['glucose_conc'] == 0])))
+print("# rows missing diastolic_bp: {0}".format(len(data_frame.loc[data_frame['diastolic_bp'] == 0])))
+print("# rows missing thickness: {0}".format(len(data_frame.loc[data_frame['thickness'] == 0])))
+print("# rows missing insulin: {0}".format(len(data_frame.loc[data_frame['insulin'] == 0])))
+print("# rows missing bmi: {0}".format(len(data_frame.loc[data_frame['bmi'] == 0])))
+print("# rows missing diab_pred: {0}".format(len(data_frame.loc[data_frame['diab_pred'] == 0])))
+print("# rows missing age: {0}".format(len(data_frame.loc[data_frame['age'] == 0])))
+
