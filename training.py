a929b7e4e5741d81b0423de9c828de80bca205cd
tanvir002700 <tanvir002700@gmail.com>
2017-08-16 01:40:34 +0600

    fix header name parse

--- training.py
+++ training.py
@@ -9,9 +9,11 @@ import pandas as PANDA
 data_frame = PANDA.read_csv('cleaned_data.csv')
 
 
-feature_column_names = data_frame.columns[0:-1]
+feature_column_names = data_frame.columns[0:-1].values
 
-predicted_class_name = data_frame.columns[-1]
+predicted_class_name = [data_frame.columns[-1]]
+
+print("feature column names: ", feature_column_names)
 
 x = data_frame[feature_column_names].values
 y = data_frame[predicted_class_name].values
@@ -26,13 +28,9 @@ print("{0:0.2f}% in test set".format((len(x_test)/len(data_frame.index)) * 100))
 
 
 print("# rows in dataframe {0}".format(len(data_frame)))
-print("# rows missing glucose_conc: {0}".format(len(data_frame.loc[data_frame['glucose_conc'] == 0])))
-print("# rows missing diastolic_bp: {0}".format(len(data_frame.loc[data_frame['diastolic_bp'] == 0])))
-print("# rows missing thickness: {0}".format(len(data_frame.loc[data_frame['thickness'] == 0])))
-print("# rows missing insulin: {0}".format(len(data_frame.loc[data_frame['insulin'] == 0])))
-print("# rows missing bmi: {0}".format(len(data_frame.loc[data_frame['bmi'] == 0])))
-print("# rows missing diab_pred: {0}".format(len(data_frame.loc[data_frame['diab_pred'] == 0])))
-print("# rows missing age: {0}".format(len(data_frame.loc[data_frame['age'] == 0])))
+
+for column in feature_column_names:
+    print("#rows missing in {0} : {1}".format(column, len(data_frame.loc[data_frame[column] == 0])))
 
 
 fill_zero = Imputer(missing_values=0, strategy='mean', axis=0)
